16853d78d0b1d7f769084dacf9a14c11926f4f29
suryaarigela <surya@Suryas-MacBook-Pro.local>
2019-09-16 11:16:23 -0400

    fix for highlighting cells based on requirement 1 and 2

--- src/app/data-visualization/data-visualization.component.ts
+++ src/app/data-visualization/data-visualization.component.ts
@@ -9,12 +9,13 @@ import { MatTableDataSource, MatPaginator, MatSort } from '@angular/material';
 })
 export class DataVisualizationComponent implements OnInit {
   displayData = false;
+  map: Map<any, any> = new Map;
   results: any[];
   profiles: any[] = [];
   keys: Set<string> = new Set;
   displayedColumns = ['time'];
   dataSource = new MatTableDataSource();
-  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
   constructor(private readonly dataServ: DataServiceService) {
     this.dataServ.listResults$.subscribe(data => {
       this.results = data;
@@ -44,9 +45,9 @@ export class DataVisualizationComponent implements OnInit {
           temp[row.symbol] = row[key];
 
         })
+        this.map.set(temp.source, temp);
         this.dataSource.data.push(temp);
       })
-
 
       this.results.forEach(row => {
         this.profiles.push(row.symbol);
@@ -70,10 +71,61 @@ export class DataVisualizationComponent implements OnInit {
     this.dataSource.sort = this.sort;
   }
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
 
   ngOnInit() {
     this.dataServ.fetchData()
+  }
+
+  getHighightedClass(cellValue: string, source: any, profile: any): string {
+    let crossBreak = false;
+    if (cellValue) {
+      let values: string[] = cellValue.split(',');
+      if (values.length > 1) {
+        let temp = this.map.get(source.source);
+        let currValue = temp[profile].split(',')[2]
+        let time = source.source.trim();
+        let pieces = time.split(':')
+        let nextTime = Number(pieces[1]) + 1;
+        let previousTemp = this.map.get(pieces[0] + ':' + nextTime.toString());
+        if (previousTemp) {
+          let previousProf = previousTemp[profile];
+          if (previousProf) {
+
+            let preValue = previousProf.split(',')[2]
+            let finalValue: number = Number(preValue) - Number(currValue)
+            if (finalValue > 0.3) {
+              crossBreak = true;
+            }
+
+          }
+
+        }
+
+        if (Number(values[0].trim()) > Number(values[1].trim())) {
+          if (crossBreak) {
+            return 'greenClass_crossBreak'
+          } else {
+            return 'greenClass'
+          }
+
+        } else {
+          if (crossBreak) {
+            return 'blueClass_crossBreak'
+          } else {
+            return 'blueClass'
+          }
+
+        }
+      } else {
+        return 'blueClass'
+      }
+    } else {
+      return 'blueClass'
+    }
+
+
+
   }
 
 }
